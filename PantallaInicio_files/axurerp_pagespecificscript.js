7bc634afe8836d90a9d97b6012887edd0311d92e
Guilhem Got <guilhem.got@gmail.com>
2013-06-24 22:18:20 +0200

    Pantalla Marisa

---
+++ PantallaInicio_files/axurerp_pagespecificscript.js
@@ -0,0 +1,7 @@
+﻿for(var i = 0; i < 60; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u52'] = 'center';gv_vAlignTable['u54'] = 'center';gv_vAlignTable['u56'] = 'center';gv_vAlignTable['u57'] = 'top';gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u30'] = 'top';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u37'] = 'top';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u44'] = 'top';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u18'] = 'center';
--- data/sitemap.js
+++ data/sitemap.js
@@ -2,7 +2,7 @@
 
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q,i,[_(c,r,e,f,g,s)])]),_(c,t,e,f,g,u)]);}; 
-var b="rootNodes",c="pageName",d="Principalproyecto",e="type",f="Wireframe",g="url",h="Principalproyecto.html",i="children",j="EdicionHistoria",k="EdicionHistoria.html",l="BacklogPequeñoV2",m="BacklogPequenoV2.html",n="BacklogGrande",o="BacklogGrande.html",p="TareasGrande",q="TareasGrande.html",r="Contenido tareas",s="Contenido_tareas.html",t="PrincipalproyectoCabRed",u="PrincipalproyectoCabRed.html";
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j,k,[_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s,k,[_(c,t,e,f,g,u)])]),_(c,v,e,f,g,w)]);}; 
+var b="rootNodes",c="pageName",d="PantallaInicio",e="type",f="Wireframe",g="url",h="PantallaInicio.html",i="Principalproyecto",j="Principalproyecto.html",k="children",l="EdicionHistoria",m="EdicionHistoria.html",n="BacklogPequeñoV2",o="BacklogPequenoV2.html",p="BacklogGrande",q="BacklogGrande.html",r="TareasGrande",s="TareasGrande.html",t="Contenido tareas",u="Contenido_tareas.html",v="PrincipalproyectoCabRed",w="PrincipalproyectoCabRed.html";
 return _creator();
 })();
