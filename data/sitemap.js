ab78db722e1ba555780f5ed472396c78b48f5f4e
Guilhem Got <guilhem.got@gmail.com>
2013-06-25 14:30:38 +0200

    Integracion Sprint y Tareas

--- data/sitemap.js
+++ data/sitemap.js
@@ -2,7 +2,7 @@
 
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j,k,[_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u,k,[_(c,v,e,f,g,w)]),_(c,x,e,f,g,y),_(c,z,e,f,g,A)]),_(c,B,e,f,g,C)]);}; 
-var b="rootNodes",c="pageName",d="PantallaInicio",e="type",f="Wireframe",g="url",h="PantallaInicio.html",i="Principalproyecto",j="Principalproyecto.html",k="children",l="EdicionHistoria",m="EdicionHistoria.html",n="BacklogPequeñoV2",o="BacklogPequenoV2.html",p="BacklogGrande",q="BacklogGrande.html",r="TareasPequeñas",s="TareasPequenas.html",t="TareasGrande",u="TareasGrande.html",v="Contenido tareas",w="Contenido_tareas.html",x="SprintPequeño",y="SprintPequeno.html",z="SprintGrande",A="SprintGrande.html",B="PrincipalproyectoCabRed",C="PrincipalproyectoCabRed.html";
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l,m,[_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w,m,[_(c,x,e,f,g,y)]),_(c,z,e,f,g,A),_(c,B,e,f,g,C)]),_(c,D,e,f,g,E)]);}; 
+var b="rootNodes",c="pageName",d="Objetivo de hoy",e="type",f="Wireframe",g="url",h="Objetivo_de_hoy.html",i="PantallaInicio",j="PantallaInicio.html",k="Principalproyecto",l="Principalproyecto.html",m="children",n="EdicionHistoria",o="EdicionHistoria.html",p="BacklogPequeñoV2",q="BacklogPequenoV2.html",r="BacklogGrande",s="BacklogGrande.html",t="TareasPequeñas",u="TareasPequenas.html",v="TareasGrande",w="TareasGrande.html",x="Contenido tareas",y="Contenido_tareas.html",z="SprintPequeño",A="SprintPequeno.html",B="SprintGrande",C="SprintGrande.html",D="PrincipalproyectoCabRed",E="PrincipalproyectoCabRed.html";
 return _creator();
 })();
