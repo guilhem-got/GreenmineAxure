f0ce5f206f447016b5684b760c8bcf51815ed1b8
Guilhem Got <guilhem.got@gmail.com>
2013-06-25 14:34:47 +0200

    Integracion Sprint y Tareas presentacion

---
+++ ObjetivoHoy_files/axurerp_pagespecificscript.js
@@ -0,0 +1,18 @@
+﻿for(var i = 0; i < 5; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u0'] = 'top';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u2'] = 'top';document.getElementById('u3_img').tabIndex = 0;
+
+u3.style.cursor = 'pointer';
+$axure.eventManager.click('u3', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('PantallaInicio.html');
+
+}
+});
+gv_vAlignTable['u4'] = 'center';
